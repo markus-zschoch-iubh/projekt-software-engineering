0aea9898b5caa0681d6cdeff2b84eecfa0294a09
Markus Zschoch <markus.zschoch@iubh-fernstudium.de>
2024-01-04 18:05:38 +0100

    Zeige offene und ausgewähltem Tutor zugeordnete Korrekturen.

--- src/backend/views.py
+++ src/backend/views.py
@@ -18,10 +18,15 @@ def tutor_overview(request):
 
 
 def tutor_index(request, tutor_id):
-    korrekturen = Korrektur.objects.all()
+    tutor = Tutor.objects.get(pk=tutor_id)
+    offene_korrekturen = Korrektur.objects.filter(aktuellerStatus="01")
+    meine_korrekturen = Korrektur.objects.filter(bearbeiter=tutor)
 
     return render(
         request,
         "backend/tutor_index.html",
-        context={"korrekturen": korrekturen},
+        context={
+            "korrekturen": offene_korrekturen,
+            "meine_korrekturen": meine_korrekturen,
+        },
     )
