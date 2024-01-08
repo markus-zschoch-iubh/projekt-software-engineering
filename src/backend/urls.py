6ef6c8663102fb910d83a12edb379f21f9aaac41
Markus Zschoch <markus.zschoch@iubh-fernstudium.de>
2024-01-08 19:35:52 +0100

    Tutoren backend

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path("", views.tutor_overview, name="tutor_overview"),
     path("<int:tutor_id>", views.tutor_index, name="tutor_index"),
+    path(
+        "<int:tutor_id>/<int:korrektur_id>",
+        views.korrektur_bearbeiten,
+        name="korrektur_bearbeiten",
+    ),
 ]
--- src/backend/views.py
+++ src/backend/views.py
@@ -19,14 +19,37 @@ def tutor_overview(request):
 
 def tutor_index(request, tutor_id):
     tutor = Tutor.objects.get(pk=tutor_id)
+    print(f"This Tutor: {tutor}")
+    if request.method == "POST":
+        zugewiesene_korrektur = Korrektur.objects.get(
+            id=request.POST.get("korrektur_id", "")
+        )
+        print(f"Zugewiesene Korrektur: {zugewiesene_korrektur}")
+        zugewiesene_korrektur.bearbeiter = tutor
+        zugewiesene_korrektur.aktuellerStatus = "02"
+        zugewiesene_korrektur.save()
+        print(f"Zugewiesene Korrektur nach Zuweisung: {zugewiesene_korrektur}")
+
     offene_korrekturen = Korrektur.objects.filter(aktuellerStatus="01")
     meine_korrekturen = Korrektur.objects.filter(bearbeiter=tutor)
 
     return render(
         request,
-        "backend/tutor_index.html",
+        "backend/tutor-index.html",
         context={
             "korrekturen": offene_korrekturen,
             "meine_korrekturen": meine_korrekturen,
+            "tutor": tutor,
         },
+    )
+
+
+def korrektur_bearbeiten(request, tutor_id, korrektur_id):
+    tutor = Tutor.objects.get(pk=tutor_id)
+    korrektur = Korrektur.objects.get(pk=korrektur_id)
+
+    return render(
+        request,
+        "backend/korrektur-bearbeiten.html",
+        context={"korrektur": korrektur, "tutor": tutor},
     )
