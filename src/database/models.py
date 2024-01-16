9ece21dc3e2b9af7a55f7f26855f9213f41fe22c
Markus Zschoch <markus.zschoch@iubh-fernstudium.de>
2024-01-16 20:32:16 +0100

    Kursmaterial Ausgabe nur Typ

--- src/database/models.py
+++ src/database/models.py
@@ -25,7 +25,7 @@ class Kursmaterial(models.Model):
         unique_together = ["typ", "kurs"]
 
     def __str__(self):
-        return f"{self.get_typ_display()} - {self.kurs}"
+        return f"{self.get_typ_display()}"
 
 
 class Student(models.Model):
